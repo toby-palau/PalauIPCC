0525f126c144d97f6428d063b4ae049c44b02df0
Toby Zethoven <toby@palauproject.com>
2023-11-01 14:41:03 +0100

    add arrow key navigation

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { MultipleChoiceQuestion } from '@root/components/MultipleChoiceQuestion'
 import { PageType, SessionType } from '@root/types/shared.types';
 import { digitalStrip, dmsans } from '@root/styles/fonts';
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useCallback } from 'react';
 import Confetti from '@root/components/Confetti';
 import data from "@root/public/question-flow.json";
 
@@ -14,24 +14,16 @@ export default function Home() {
 	const [confetti, setConfetti] = useState<boolean>(false);
 	const [currentPage, setCurrentPage] = useState<PageType>();
 
-	useEffect(() => { fetchQuestions() }, []);
+	useEffect(() => { setSession({pages: data.pages as Array<PageType>}) }, []);
 	useEffect(() => { setCurrentPage(session?.pages.find(q => q.qid === currentId)) }, [session?.pages, currentId]);
-
 	useEffect(() => {
 		const handleKeyPressEvent = (e: KeyboardEvent) => {
-			console.log(e.key)
 			if (e.key === "ArrowRight") navigate(currentId, "forward");
 			if (e.key === "ArrowLeft") navigate(currentId, "backward");
-		}
-		addEventListener("keydown", console.log);
-		// return () => removeEventListener("keydown", handleKeyPressEvent);
-	  }, []);
-	
-	const fetchQuestions = async () => {
-		// const response = await fetch(`http://localhost:3000/api/questions`);
-		// const {data} = await response.json();
-		setSession({pages: data.pages as Array<PageType>});
-	}
+		};
+		addEventListener("keydown", handleKeyPressEvent);
+		return () => removeEventListener("keydown", handleKeyPressEvent);
+	}, [currentId, session]);
 	
 	const navigate = (fromId: number, direction: "forward" | "backward", newSession?: SessionType) => {
 		const thisSession = newSession || session;
@@ -54,7 +46,6 @@ export default function Home() {
 		const logicQuestion = thisSession.pages.find(q => q.qid === logicQuestionId);
 		if (!logicQuestion?.question) return setCurrentId(toId);
 
-		
 		const correct = logicQuestion.question.selection.sort().join() === logicQuestion.question.correct.sort().join();
 		if (correct === newQuestion.displayLogic.correct) setCurrentId(toId)
 		else navigate(toId, direction, newSession);
@@ -80,70 +71,68 @@ export default function Home() {
 
 	if (session && currentPage) {
 		return (
-			<div className="h-screen w-screen">
-				<div className="fixed h-screen w-screen flex-col items-center justify-center overflow-y-scroll">
-					<div
-						className="fixed h-screen w-screen"
-					>
-						<img 
-							src={`/images/backgrounds/${currentPage.backgroundImage}`}
-							alt="background image"
-							className="h-full w-full object-cover brightness-50"
-							onClick={() => navigate(currentId, "forward")}
-						/>
-						{ !currentPage.question && (
-							<div className="absolute flex flex-row items-center bottom-10 right-10 pointer-events-none">
-								<p className="text-white text-right w-48">{"click anywhere to go to the next page!"}</p>
-								<img
-									src={"/images/misc/finger.gif"}
-									alt="click to continue"
-									className="h-16 w-16"
-								/>
-							</div>
-						) }
-					</div>
-					
-					{ currentPage.question && (
-						<div className="absolute w-full min-h-full py-24 sm:px-20 px-5 flex justify-center align-center">
-							{ currentPage.question && <MultipleChoiceQuestion question={currentPage.question} submitResponse={(selection) => submitResponse(currentPage.qid, selection)} /> }
-						</div>
-					) }
-					<div className="fixed w-full flex flex-col items-start">
-						<div className="h-2 w-full bg-white opacity-80">
-							<div className="h-full bg-blue" style={{width: `${session.pages.findIndex(p => p.qid === currentId) / session.pages.length * 100}%`}}/>
-						</div>
-						{ currentId > 1 ? (
-							<div 
-								className="flex flex-col m-2 items-center cursor-pointer active:scale-95 active:opacity-80"
-								onClick={() => navigate(currentId, "backward")}	
-							>
-								<img
-									src={`/images/misc/arrow-left-bold.svg`}
-									alt="back"
-									className="sm:h-12 sm:w-12 h-8 w-8 pointer-events-none"
-								/>
-								<p className={`${dmsans.className} text-white sm:text-base text-sm`}>{"BACK"}</p>
-							</div>
-						) : null }
-					</div>
-					<div className="fixed sm:bottom-10 sm:left-10 bottom-4 left-4 pointer-events-none">
-						{
-							currentPage.avatarText && (
-								<div className="sm:w-96 sm:max-w-96 w-64 mb-4 p-4 relative border-2 border-black rounded bg-white">
-									<p className={`${digitalStrip.className} sm:text-base text-sm text-black`}>
-										{currentPage.avatarText}
-									</p>
-								</div>
-							)
-						}
-						<img 
-							src={`/images/avatars/${currentPage.avatarImage}`}
-							alt="avatar" 
-							className={`${currentPage.question && "scale-50"} sm:h-56 sm:w-56 h-24 w-24 rounded-full border-2 border-white transition-all duration-500 delay-75`}
-						/>
-					</div>
-					{confetti && <Confetti />}
+			<div className="fixed h-screen w-screen flex-col items-center justify-center overflow-y-scroll">
+				<div
+					className="fixed h-screen w-screen"
+				>
+					<img 
+						src={`/images/backgrounds/${currentPage.backgroundImage}`}
+						alt="background image"
+						className="h-full w-full object-cover brightness-50"
+						onClick={() => navigate(currentId, "forward")}
+					/>
 				</div>
+				
+				{ currentPage.question && (
+					<div className="absolute w-full min-h-full py-24 md:px-20 px-5 flex justify-center align-center">
+						{ currentPage.question && <MultipleChoiceQuestion question={currentPage.question} submitResponse={(selection) => submitResponse(currentPage.qid, selection)} /> }
+					</div>
+				) }
+				<div className="fixed w-full flex flex-col items-start">
+					<div className="h-2 w-full bg-white opacity-80">
+						<div className="h-full bg-blue" style={{width: `${session.pages.findIndex(p => p.qid === currentId) / session.pages.length * 100}%`}}/>
+					</div>
+					{ currentId > 1 ? (
+						<div 
+							className="flex flex-col m-2 items-center cursor-pointer active:scale-95 active:opacity-80"
+							onClick={() => navigate(currentId, "backward")}	
+						>
+							<img
+								src={`/images/misc/arrow-left-bold.svg`}
+								alt="back"
+								className="md:h-12 md:w-12 h-8 w-8 pointer-events-none"
+							/>
+							<p className={`${dmsans.className} text-white md:text-base text-sm`}>{"BACK"}</p>
+						</div>
+					) : null }
+				</div>
+				<div className="fixed md:bottom-10 md:left-10 bottom-3 left-3 pointer-events-none">
+					{
+						currentPage.avatarText && (
+							<div className="md:w-96 md:max-w-96 w-64 mb-4 p-4 relative border-2 border-black rounded bg-white">
+								<p className={`${digitalStrip.className} md:text-base text-sm text-black`}>
+									{currentPage.avatarText}
+								</p>
+							</div>
+						)
+					}
+					<img 
+						src={`/images/avatars/${currentPage.avatarImage}`}
+						alt="avatar" 
+						className={`${currentPage.question && "scale-50"} md:h-56 md:w-56 h-24 w-24 rounded-full border-2 border-white transition-all duration-500 delay-75`}
+					/>
+				</div>
+				{ !currentPage.question && (
+					<div className="absolute flex flex-row items-end bottom-6 right-6 md:bottom-10 md:right-10 pointer-events-none">
+						<p className="text-white text-right md:text-base text-sm md:w-48 w-32">{"click anywhere to go to the next page!"}</p>
+						<img
+							src={"/images/misc/finger.gif"}
+							alt="click to continue"
+							className="h-8 w-8 md:h-16 md:w-16"
+						/>
+					</div>
+				) }
+				{confetti && <Confetti />}
 			</div>
 		)
 	} else return null;
--- components/MultipleChoiceQuestion.tsx
+++ components/MultipleChoiceQuestion.tsx
@@ -10,7 +10,7 @@ export const MultipleChoiceQuestion = (props: {question: QuestionType, submitRes
             <div className="flex flex-col items-center justify-between">
                 {props.question.options.map(o => {
                     let className = `${dmsans.className} w-full cursor-pointer sm:text-xl text-md text-center text-black rounded-md my-0.5 p-3 active:scale-95 transition-all duration-100`;
-                    if (props.question.selection.includes(o.oid) && props.question.correct.includes(o.oid)) className += " bg-green hover:bg-green-dark text-white";
+                    if (props.question.selection.length > 0 && props.question.correct.includes(o.oid)) className += " bg-green hover:bg-green-dark text-white";
                     else if (props.question.selection.includes(o.oid) && !props.question.correct.includes(o.oid)) className += " bg-red hover:bg-red-dark text-white animate-wiggle";
                     else className += " bg-white hover:bg-white-dark text-black";
                     return (
