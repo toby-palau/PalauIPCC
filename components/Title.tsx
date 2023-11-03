5f93da724aec7f9f8730628d55cab4b5409fd2c0
Toby Zethoven <toby@palauproject.com>
2023-11-03 20:33:40 +0100

    fix title page mobile layout

--- components/Title.tsx
+++ components/Title.tsx
@@ -4,10 +4,10 @@ import { dmsans, staatliches } from "@root/styles/fonts"
 
 export const Title = (props: {title: string, subtitle?: string}) => {
     return (
-        <div className="absolute flex flex-col justify-center items-center h-full w-full pointer-events-none">
-            <h1 className={`${staatliches.className} text-6xl text-white`}>{props.title}</h1>
+        <div className="absolute flex flex-col justify-center items-center h-full w-full pointer-events-none p-2">
+            <h1 className={`${staatliches.className} md:text-6xl text-4xl text-white text-center`}>{props.title}</h1>
             { props.subtitle && (
-                <h2 className={`${dmsans.className} text-2xl text-white`}>{props.subtitle}</h2>
+                <h2 className={`${dmsans.className} md:text-2xl text-xl text-white text-center`}>{props.subtitle}</h2>
             ) }
         </div>
     )
