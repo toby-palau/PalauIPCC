bbfb94bcf69ebc6e863e539d3a3bd6965c41ec17
Toby Zethoven <toby@palauproject.com>
2023-11-10 13:55:30 +0100

    Use next Metadata config instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,18 +7,19 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
 	title: 'Palau - IPCC 2023',
 	description: 'Palau created this summary of the 2023 IPCC report.',
+	openGraph: {
+		title: "Palau - IPCC 2023",
+		description: "Palau created this summary of the 2023 IPCC report.",
+		images: [
+			`http://${process.env.URL}/images/misc/og-image.png`,
+			`https://${process.env.URL}/images/misc/og-image.png`,
+		],
+	}
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<head>
-				<meta property="og:image" content={`http://${process.env.URL}/images/misc/og-image.png`} />
-				<meta property="og:image:secure_url" content={`https://${process.env.URL}/images/misc/og-image.png`} /> 
-				<meta property="og:image:type" content="image/png" /> 
-				<meta property="og:image:width" content="4764" /> 
-				<meta property="og:image:height" content="2452" />
-			</head>
 			<body className={`${inter.className} overscroll-none`}>{children}</body>
 		</html>
 	)
