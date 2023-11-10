c7b59b32ccc0d5a2db9f0ef06bf7a1433405e7cb
Toby Zethoven <toby@palauproject.com>
2023-11-10 17:33:47 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,7 +19,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className} overscroll-none`}>{children}</body>
+			<body className={`${inter.className} overscroll-none`}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	)
 }
