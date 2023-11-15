ca6714ea0d75ad782af79ff74b6cf3aea85037aa
Toby Zethoven <toby@palauproject.com>
2023-11-15 11:22:55 +0100

    redirect to next chapter

--- app/chapters/[chapterId]/actions.ts
+++ app/chapters/[chapterId]/actions.ts
@@ -6,15 +6,16 @@ import { ChapterType, QuestionFlowType } from '@root/@types/shared.types';
 
 const filePath = path.resolve("data", "question-flow.json");
 
-export const getChapter: (chapterId: string) => ChapterType | undefined = (chapterId) => {
+export const getChapter: (chapterId: string) => {chapter?: ChapterType; nextChapterId?: string} = (chapterId) => {
     try {
         const fileContents = fs.readFileSync(filePath, 'utf8');
         const { chapters } = JSON.parse(fileContents) as QuestionFlowType;
         const chapter = chapters.find(c => c.cid === chapterId);
         if (!chapter) throw new Error('Chapter not found');
-        return chapter;
+        const nextChapterId = chapters[chapters.findIndex(c => c.cid === chapterId) + 1]?.cid;
+        return { chapter, nextChapterId};
     } catch (error) {
         console.log(error);
-        return;
+        return {chapter: undefined, nextChapterId: undefined};
     }
 }
