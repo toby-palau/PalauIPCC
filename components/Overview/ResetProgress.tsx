e5cf625be9ce4d3263defed62835f45b67670983
Toby Zethoven <toby@palauproject.com>
2023-11-17 13:43:32 +0100

    refresh page after reset click

--- components/Overview/ResetProgress.tsx
+++ components/Overview/ResetProgress.tsx
@@ -2,12 +2,20 @@
 
 import { deleteAllResponses } from "@root/services/DatabaseService"
 import { dmsans } from "@root/styles/fonts"
+import { useRouter } from "next/navigation"
 
 export const ResetProgress = ({userId}: {userId: string}) => {
+    const router = useRouter();
+
+    const handleResetClick = () => {
+        deleteAllResponses(userId);
+        router.refresh();
+    }
+
     return (
         <button 
             className={`${dmsans.className} w-full text-center mt-4 text-red text-sm hover:underline active:scale-95 transition-all duration-100`}
-            onClick={() => deleteAllResponses(userId)}
+            onClick={handleResetClick}
         >
             Reset my progress
         </button>
