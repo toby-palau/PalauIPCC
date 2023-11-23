367dc1cf735c822f19c6ccb752091a0faab6b7de
Toby Zethoven <toby@palauproject.com>
2023-11-23 15:48:31 +0100

    remove middleware and uncomment database code

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -4,15 +4,14 @@ import { updateUserCountry } from "@root/services/DatabaseService";
 
 export async function POST(request: NextRequest) {
     try {
-        // const userId = await request.json();
-        // if (!userId) throw new Error("No user id provided");
+        const userId = await request.json();
+        if (!userId) throw new Error("No user id provided");
         const { city, country, flag, countryRegion, region, latitude, longitude } = geolocation(request);
         console.log({ route: { city, country, flag, countryRegion, region, latitude, longitude } })
-        return NextResponse.json({status: 200, body: { city, country, flag, countryRegion, region, latitude, longitude }});
-        // if (!country) throw new Error("Country not found");
-        // const user = await updateUserCountry(userId, country);
-        // if (!user) throw new Error("Unable to update user country");
-        // return NextResponse.json({status: 200, body: { user }});
+        if (!country) throw new Error("Country not found");
+        const user = await updateUserCountry(userId, country);
+        if (!user) throw new Error("Unable to update user country");
+        return NextResponse.json({status: 200, body: { user }});
 
     } catch (error) {
         console.log(error);
