d5f29c0ac0ddd341bdd97cc02cee05e0bdeab48f
Toby Zethoven <toby@palauproject.com>
2023-11-29 16:14:06 +0100

    add some meta tags for indexing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
-import { Analytics } from '@vercel/analytics/react';
-import type { Metadata } from 'next'
-import { DM_Sans } from 'next/font/google'
-import './globals.css'
+import { Analytics } from "@vercel/analytics/react";
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+import "./globals.css";
 
 
-const inter = DM_Sans({ subsets: ['latin'] })
+const inter = DM_Sans({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
 	metadataBase: new URL(process.env.URL ?? "https://ipcc.palauproject.com"),
-	title: 'IPCC for dummies',
-	description: 'Explore bite-sized chapters of the IPCC report in a gamified format. Learn about expert strategies for adaptation and mitigation without being overwhelmed by dense research papers.',
+	title: "IPCC for dummies",
+	description: "Explore bite-sized chapters of the IPCC report in a gamified format. Learn about expert strategies for adaptation and mitigation without being overwhelmed by dense research papers.",
+	keywords: ["Climate change education", "IPCC 2023", "Gamified learning", "Interactive climate scenarios", "Sustainability", "Educational games", "Climate action", "Environmental challenges"],
+	verification: {
+		google: ["xWmWb_2Fjr2R6QRZjfgvYcO0fwaecB7iRHL_4gkMQkk"]
+	},
 	openGraph: {
 		title: "IPCC for dummies",
 		description: "Explore bite-sized chapters of the IPCC report in a gamified format. Learn about expert strategies for adaptation and mitigation without being overwhelmed by dense research papers.",
