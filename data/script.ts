c1f6952d7abbad928a3abc075d031775e250aa96
shendric-lamar <simon@palauproject.com>
2023-12-17 14:41:22 +0100

    Add image names to cop-question-flow

--- data/script.ts
+++ data/script.ts
@@ -2,21 +2,21 @@ const fs = require("fs");
 const path = require("path");
 const { ulid } = require("ulidx");
 
-const generateLookupFile = () => {
-    const jsonFilePath = path.resolve("data", "ipcc-question-flow.json");
-    const fileContents = fs.readFileSync(jsonFilePath, "utf8");
-    const { chapters } = JSON.parse(fileContents);
+// const generateLookupFile = () => {
+//     const jsonFilePath = path.resolve("data", "ipcc-question-flow.json");
+//     const fileContents = fs.readFileSync(jsonFilePath, "utf8");
+//     const { chapters } = JSON.parse(fileContents);
 
-    const csvFilePath = path.resolve("data", "lookup.csv");
-    const csvFile = fs.createWriteStream(csvFilePath);
-    csvFile.write("questionID,questionText,chapterId,chapterTitle\n");
+//     const csvFilePath = path.resolve("data", "lookup.csv");
+//     const csvFile = fs.createWriteStream(csvFilePath);
+//     csvFile.write("questionID,questionText,chapterId,chapterTitle\n");
 
-    for (const chapter of chapters) {
-        for (const page of chapter.pages) {
-            if (page.pageType === "question") csvFile.write(`"${page.pid}","${page.question.questionText}","${chapter.cid}","${chapter.chapterTitle}"\n`);
-        }
-    }
-}
+//     for (const chapter of chapters) {
+//         for (const page of chapter.pages) {
+//             if (page.pageType === "question") csvFile.write(`"${page.pid}","${page.question.questionText}","${chapter.cid}","${chapter.chapterTitle}"\n`);
+//         }
+//     }
+// }
 
 const regenerateIds = () => {
     const jsonFilePath = path.resolve("data", "cop-question-flow.json");
@@ -37,4 +37,4 @@ const regenerateIds = () => {
     fs.writeFileSync(jsonFilePath, JSON.stringify({ chapters }, null, 4));
 }
 
-// regenerateIds();
+regenerateIds();
