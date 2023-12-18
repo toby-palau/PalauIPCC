bcfe2ce44a55208917dafa6754a98f0f9ea52e21
Toby Zethoven <toby@palauproject.com>
2023-12-18 14:19:57 +0100

    fix metadata og:image

--- app/[quizId]/layout.tsx
+++ app/[quizId]/layout.tsx
@@ -9,7 +9,7 @@ const inter = DM_Sans({ subsets: ["latin"] })
 
 export const generateMetadata = ({ params }: { params: { quizId: QuizIdType } }): Metadata => {
 	if (params.quizId === "ipcc") return {
-		metadataBase: new URL(`${process.env.URL ?? "https://quiz.palauproject.com"}/ipcc`),
+		metadataBase: new URL(`${process.env.URL ?? "https://quiz.palauproject.com"}`),
 		title: "IPCC for dummies",
 		description: "Explore bite-sized chapters of the IPCC report in a gamified format. Learn about expert strategies for adaptation and mitigation without being overwhelmed by dense research papers.",
 		keywords: ["Climate change education", "IPCC 2023", "Gamified learning", "Interactive climate scenarios", "Sustainability", "Educational games", "Climate action", "Environmental challenges"],
@@ -23,7 +23,7 @@ export const generateMetadata = ({ params }: { params: { quizId: QuizIdType } })
 		}
 	}
 	else return {
-		metadataBase: new URL(`${process.env.URL ?? "https://quiz.palauproject.com"}/cop`),
+		metadataBase: new URL(`${process.env.URL ?? "https://quiz.palauproject.com"}`),
 		title: "COP28 Recap",
 		description: "Dive into the key topics of COP28 with this engaging quiz. Test your knowledge on climate action goals, renewable energy, and global challenges in a concise and informative way.",
 		keywords: ["Climate change education", "COP28", "Gamified learning", "Interactive climate scenarios", "Sustainability", "Educational games", "Climate action", "Environmental challenges"],
