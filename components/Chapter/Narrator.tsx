f12967e1d5f4abc8dcde72fbdb1f999aacacebb3
Toby Zethoven <toby@palauproject.com>
2024-02-28 16:33:34 +0100

    fix narrator layout

--- components/Chapter/Narrator.tsx
+++ components/Chapter/Narrator.tsx
@@ -10,9 +10,9 @@ type NarratorProps = {
 
 export const Narrator = ({ avatarImage, avatarText, small }: NarratorProps) => {
 	return (
-		<div className="fixed md:p-10 md:pt-24 p-3 pt-20 pointer-events-none z-20 flex flex-col max-h-full">
+		<div className="fixed md:p-10 md:pt-24 p-3 pt-20 pointer-events-none z-20 flex flex-col justify-end h-full">
 			{avatarText && (
-				<div className="pointer-events-auto md:w-96 md:max-w-96 w-64 mb-4 p-4 relative border-2 border-black rounded bg-white flex-1 overflow-y-scroll">
+				<div className="pointer-events-auto md:w-96 md:max-w-96 w-64 mb-4 p-4 relative border-2 border-black rounded bg-white overflow-y-scroll">
 					<p
 						className={`${digitalStrip.className} md:text-base text-sm text-black`}
 					>
@@ -25,7 +25,7 @@ export const Narrator = ({ avatarImage, avatarText, small }: NarratorProps) => {
 				alt="avatar"
 				className={`${
 					small && "scale-50"
-				} md:h-56 md:w-56 h-24 w-24 rounded-full border-2 border-white transition-all duration-500 delay-75 flex-1`}
+				} md:h-56 md:w-56 h-24 w-24 rounded-full border-2 border-white transition-all duration-500 delay-75`}
 			/>
 		</div>
 	);
