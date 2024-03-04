b297d5b3800f58ab290e9298edb430a8d66eaa52
Toby Zethoven <toby@palauproject.com>
2024-03-04 16:47:58 +0100

    add script for inserting display logic

--- data/script.ts
+++ data/script.ts
@@ -19,25 +19,73 @@ const { ulid } = require("ulidx");
 // }
 
 const regenerateIds = () => {
-    const jsonFilePath = path.resolve("data", "uob-question-flow.json");
-    const fileContents = fs.readFileSync(jsonFilePath, "utf8");
-    const { chapters } = JSON.parse(fileContents);
-    for (const chapter of chapters) {
-        chapter.cid = `CID_${ulid()}`;
-        for (const page of chapter.pages) {
-            page.pid = `PID_${ulid()}`;
-            if (page.pageType === "question" && page.question.options) {
-                const optionMapping: {[oldOid: string]: string} = {};
-                for (const option of page.question.options) {
-                    const newOid = `OID_${ulid()}`;
-                    optionMapping[option.oid] = newOid;
-                    option.oid = newOid;
-                }
-                page.question.correctAnswer = page.question.correctAnswer.map((o: string) => optionMapping[o]);
-            }
-        }
-    }
-    fs.writeFileSync(jsonFilePath, JSON.stringify({ chapters }, null, 4));
-}
+	const jsonFilePath = path.resolve("data", "uob-question-flow.json");
+	const fileContents = fs.readFileSync(jsonFilePath, "utf8");
+	const { chapters } = JSON.parse(fileContents);
+	for (const chapter of chapters) {
+		chapter.cid = `CID_${ulid()}`;
+		for (const page of chapter.pages) {
+			page.pid = `PID_${ulid()}`;
+			if (page.pageType === "question" && page.question.options) {
+				const optionMapping: { [oldOid: string]: string } = {};
+				for (const option of page.question.options) {
+					const newOid = `OID_${ulid()}`;
+					optionMapping[option.oid] = newOid;
+					option.oid = newOid;
+				}
+				page.question.correctAnswer = page.question.correctAnswer.map(
+					(o: string) => optionMapping[o]
+				);
+			}
+		}
+	}
+	fs.writeFileSync(jsonFilePath, JSON.stringify({ chapters }, null, 4));
+};
 
-regenerateIds();
+const fillInDisplayLogicIds = () => {
+	const jsonFilePath = path.resolve("data", "uob-question-flow.json");
+	const fileContents = fs.readFileSync(jsonFilePath, "utf8");
+	const { chapters } = JSON.parse(fileContents);
+	for (const chapter of chapters) {
+		let i = 0;
+		for (const page of chapter.pages) {
+			console.log({ pageId: page.pid, pageType: page.pageType });
+			if (page.pageType === "question") {
+				if (
+					chapter.pages[i + 1]?.pageType !== "narrator" ||
+					chapter.pages[i + 2]?.pageType !== "narrator"
+				) {
+					console.log("Expected narrator pages after question");
+					continue;
+				}
+
+				if (
+					!chapter.pages[i + 1]?.displayLogic ||
+					!chapter.pages[i + 2]?.displayLogic
+				) {
+					console.log(
+						"Expected narrator pages to not have display logic"
+					);
+					continue;
+				}
+				chapter.pages[i + 1].displayLogic = {
+					pid: page.pid,
+					type: "answeredCorrectly",
+					correct: true,
+				};
+				chapter.pages[i + 2].displayLogic = {
+					pid: page.pid,
+					type: "answeredCorrectly",
+					correct: false,
+				};
+				console.log(`Updated display logic for question ${page.pid}`);
+			}
+			i++;
+		}
+	}
+	fs.writeFileSync(jsonFilePath, JSON.stringify({ chapters }, null, 4));
+};
+
+fillInDisplayLogicIds();
+
+// regenerateIds();
